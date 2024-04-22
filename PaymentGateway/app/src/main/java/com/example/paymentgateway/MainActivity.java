a77655f5a0a239cb76d4e81f84c1e575dd70ce09
Lakshya Goel <lgoel340@gmail.com>
2024-04-22 11:27:13 +0530

    Payment Gateway

---
+++ PaymentGateway/app/src/main/java/com/example/paymentgateway/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.paymentgateway;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements PaymentResultListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.payButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startPayment();
+            }
+        });
+        };
+
+    public void startPayment(){
+        Checkout checkout = new Checkout();
+        checkout.setImage(R.mipmap.ic_launcher);
+        final Activity activity = this;
+
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", R.string.app_name);
+            options.put("description", "Payment for Anything");
+            options.put("send_sms_hash", true);
+            options.put("allow_rotation", false);
+
+            options.put("currency", "INR");
+            options.put("amount", "100");       // 100 means 1 rupee, if anything needds to be written, then multiply with 100
+
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", "email");
+            preFill.put("contact", "phone");
+
+            options.put("prefill", preFill);
+
+            checkout.open(activity, options);
+        }
+        catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                    .show();
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(this, "Payment Successful" + s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(this, "Payment Error" + s, Toast.LENGTH_SHORT).show();
+    }
+}
